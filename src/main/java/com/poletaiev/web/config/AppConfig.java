5157fb16d2d23f528b23189875ed866a7734070e
Andrey Poletaiev <poletaiev@gmail.com>
2021-05-18 11:17:51 +0300

    Fix classpath loading for applicationContext.xml, move to resources Remove extra duplicated MVC config class

--- src/main/java/com/poletaiev/web/config/AppConfig.java
+++ src/main/java/com/poletaiev/web/config/AppConfig.java
@@ -1,13 +1,29 @@
 package com.poletaiev.web.config;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
-@ImportResource("applicationContext.xml")
-//@ComponentScan(basePackages = "com.poletaiev")
-public class AppConfig  extends WebMvcConfigurerAdapter {
+@ImportResource({"classpath*:applicationContext.xml"})
+public class AppConfig  implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
 
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver =
+                new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/pages/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
 }
